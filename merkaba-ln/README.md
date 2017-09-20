d677e89abd75c50223d591861c4345edee15aaeb
Christopher Adams <christopher@fabricatorz.com>
2017-09-20 15:24:58 +0800

    Animate SVG

--- merkaba-ln/README.md
+++ merkaba-ln/README.md
@@ -2,6 +2,11 @@
 
 ![merkaba](https://raw.githubusercontent.com/poexio/poex-visual-identity/master/merkaba-ln/out.gif?token=AADtJ7lPaJ8XlRwrQUKfaF4VjZgpme3Zks5ZybWWwA%3D%3D)
 
+## Requirements
+
+- Go
+- https://github.com/sangaline/svganimator
+
 ### Instructions
 
 ```sh
@@ -14,4 +19,11 @@ Animated GIF:
 
 ```sh
 convert -loop 0 -delay 1.5 out0*.png out.gif
+```
+
+Animated SVG:
+
+```sh
+sed -i 's|<polyline|<polyline style="stroke-width:4;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:round"|' out0*.svg
+python svganimator.py -s 0.03 -p 3 out.svg out0*.svg
 ```
--- merkaba-ln/merkaba.go
+++ merkaba-ln/merkaba.go
@@ -1,12 +1,5 @@
 package main
 
-// Instructions
-
-// go get github.com/fogleman/ln/ln
-// go build merkaba.go
-// ./merkaba
-// convert -loop 0 -delay 1 out0*.png out.gif
-
 import "fmt"
 import "github.com/fogleman/ln/ln"
 
@@ -34,41 +27,41 @@ func main() {
 
 	tetra := ln.NewMesh([]*ln.Triangle {t1, t2, t3, t4, t5, t6, t7, t8})
 
-	for i := 0; i < 90; i += 1 {
+	for i := 0; i < 90; i += 2 {
 		fmt.Println(i)
-		m := ln.Rotate(ln.Vector{0, 0, -1}, ln.Radians(float64(i)))
+		m := ln.Rotate(ln.Vector{0, 0, -2}, ln.Radians(float64(i)))
 
 		shape := ln.NewTransformedShape(tetra, m)
 
-		render(shape, fmt.Sprintf("out%03d.png", i))
+		render(shape, fmt.Sprintf("out%03d", i))
 	}
 }
 
-func render(shape ln.Shape, path string) {
+func render(shape ln.Shape, rootname string) {
 	// create a scene
 	scene := ln.Scene{}
 
 	scene.Add(shape)
 
 	// define camera parameters
-	eye := ln.Vector{4, 4, 4}    // camera position
+	eye := ln.Vector{6, 6, 6}    // camera position
 	center := ln.Vector{0, 0, 0} // camera looks at
 	up := ln.Vector{0, 0, 1}     // up direction
 
 	// define rendering parameters
 	width := 512.0   // rendered width
 	height := 512.0  // rendered height
-	fovy := 40.0     // vertical field of view, degrees
+	fovy := 20.0     // vertical field of view, degrees
 	znear := 0.1     // near z plane
-	zfar := 10.0     // far z plane
+	zfar := 20.0     // far z plane
 	step := 0.01     // how finely to chop the paths for visibility testing
 
 	// compute 2D paths that depict the 3D scene
 	paths := scene.Render(eye, center, up, width, height, fovy, znear, zfar, step)
 
 	// render the paths in an image
-	paths.WriteToPNG(path, width, height)
+	paths.WriteToPNG(rootname + ".png", width, height)
 
 	// save the paths as an svg
-	// paths.WriteToSVG("out.svg", width, height)
+	paths.WriteToSVG(rootname + ".svg", width, height)
 }
